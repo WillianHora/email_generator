5711cd3eb004ec1fc54d68e0bc8e84ee975cf48c
Willian Hora <wrsantos2002@gmail.com>
2023-01-26 08:00:51 -0300

    _domain

--- app.js
+++ app.js
@@ -8,14 +8,26 @@ let password = document.querySelector("#password")
 
 let containerPassword = document.querySelector("#container-password")
 
-let charset = "abcdefghijklmnopqrstuvwABCDEFGHIJKLMNOPQRSTUVW"
+let charset = "abcdefghijklmnopqrstuvw_."
 let novaSenha = "";
+const emailDomainButtons = document.querySelectorAll('input[name="email_domain"]');
 
 sizePassword.innerHTML = sliderElement.value;
 
 sliderElement.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
+
+
+
+
+for (let button of emailDomainButtons) {
+    button.addEventListener('change', function() {
+        selectedDomain = this.value;
+    });
+}
+
+selectedDomain = "@gmail.com"
 
 function generationPassword(){
     let pass = "";
@@ -24,10 +36,10 @@ function generationPassword(){
        
     }
     containerPassword.classList.remove("hide");
-    password.innerHTML = pass + "@hotmail.com";
+    password.innerHTML = pass + selectedDomain;
     novaSenha = pass;
 }
 
 function copyPassword(){
-   navigator.clipboard.writeText(novaSenha + "@hotmail.com")
+   navigator.clipboard.writeText(novaSenha + selectedDomain)
 }
